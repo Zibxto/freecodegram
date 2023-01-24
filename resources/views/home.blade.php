f76bc0d3777f753cd8666a0a29173fc62148c201
zibxto <christianaziba@gmail.com>
2023-01-24 10:54:02 +0100

    updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+<div class="container mt-4" style="width:50%;">
+    <div class="row">
+        <div class="col-3">
+        <img src="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/ikqra03zdnggljdu5vv0" class="rounded-circle" style="max-width: 100%; max-height:100%">
+        </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+        <div class="col-9 pl-5">
+         <div><h1>{{$user->username}}</h1></div>
 
-                    You are logged in!
-                </div>
-            </div>
+         <div class="d-flex">
+            <div class="pr-5"><strong>153</strong> posts</div>
+            <div class="pr-5"><strong>23k</strong> followers</div>
+            <div class="pr-5"><strong>212</strong> following</div>
+         </div>
+
+         <div class="pt-4 font-weight-bold">{{ $user->profile->title}}</div>
+        <div>{{ $user->profile->description}}</div>
+        <div><a href="#">{{$user->profile->url}}</a></div>
+
+        </div>
+    </div>
+
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="{{URL::asset('/image/Rohman.jpg')}}" alt="" class="w-100">
+        </div>
+         <div class="col-4">
+            <img src="{{URL::asset('/image/Rohman.jpg')}}" alt="" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="{{URL::asset('/image/Rohman.jpg')}}" alt="" class="w-100">
         </div>
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,5 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/profile/{user}', 'profilesController@index')->name('profile.show');
+
